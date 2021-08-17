5cf5f8964d39ff04c9bffe54a98adbb6e7f7f67d
Akshara Purwar <aksharapurwar@gmail.com>
2021-08-17 11:35:33 +0530

    enqueue,dequeue

---
+++ Queue.java
@@ -0,0 +1,63 @@
+package bridgelabz;
+
+public class Queue
+{
+	private Node front, rear; 
+	private int currentSize; 
+ 
+
+	private class Node{ 
+		
+		int data;
+		Node next;
+	}
+ 
+ 
+	public Queue(){
+		
+		front = null;
+		rear = null;
+		currentSize = 0;
+	}
+ 
+	public boolean isEmpty(){
+		
+		return (currentSize == 0);
+	}
+ 
+
+	public int dequeue(){
+	 
+		int data = front.data;
+		front = front.next;
+		if (isEmpty()) {
+			
+			 rear = null;
+		 }
+		
+		currentSize--;
+		System.out.println(data+ " removed from the queue");
+		return data;
+ }
+ 
+
+	public void enqueue(int data)
+	{
+		Node oldRear = rear;
+		rear = new Node();
+		rear.data = data;
+		rear.next = null;
+		if (isEmpty()) {
+			front = rear;
+		}
+		else {
+			
+			oldRear.next = rear;
+		}
+		
+		currentSize++;
+		System.out.println(data+ " added to the queue");
+	}
+ 
+}
+ 
